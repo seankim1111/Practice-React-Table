5f2a1879db597a30cf5bade6463ed34f32f65ad5
seankim1111 <yongsun199@gmail.com>
2022-02-02 22:28:51 +0900

    [Modify]추가기능수정

--- src/components/SortingTable.js
+++ src/components/SortingTable.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import { useTable, useSortBy } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
-import { COLUMNS, GROUPED_COLUMNS } from "./columns";
+import { COLUMNS } from "./columns";
 import "./table.css";
 
 export const SortingTable = () => {
