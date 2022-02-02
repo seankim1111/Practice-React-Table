a323ff34b26e4209bc39f63e19930dfcb7843656
seankim1111 <yongsun199@gmail.com>
2022-02-02 22:28:51 +0900

    [Add]More on Filtering(useAsyncDebounce)

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,6 @@
+import { format } from "date-fns";
+import { ColumnFilter } from "./ColumnFilter";
+
 export const COLUMNS = [
   {
     Header: "Id",
@@ -5,6 +8,8 @@ export const COLUMNS = [
     Footer: "Id",
     accessor: "id",
     // accessor 속성에는 해당 열을 data 객체의 어느 속성을 읽어야하는지 명시
+    // Filter: ColumnFilter,
+    disableFilters: true,
   },
   {
     Header: "First Name",
@@ -20,6 +25,9 @@ export const COLUMNS = [
     Header: "Date of Birth",
     Footer: "Date of Birth",
     accessor: "date_of_birth",
+    Cell: ({ value }) => {
+      return format(new Date(value), "dd/MM/yyyy");
+    },
   },
   {
     Header: "Country",
@@ -33,49 +41,49 @@ export const COLUMNS = [
   },
 ];
 
-export const GROUPED_COLUMNS = [
-  {
-    Header: "Id",
-    // Header 속성에는 테이블 헤더에 보여줄 텍스트를 명시
-    Footer: "Id",
-    accessor: "id",
-    // accessor 속성에는 해당 열을 data 객체의 어느 속성을 읽어야하는지 명시
-  },
-  {
-    Header: "Name",
-    Footer: "Name",
-    columns: [
-      {
-        Header: "First Name",
-        Footer: "First Name",
-        accessor: "first_name",
-      },
-      {
-        Header: "Last Name",
-        Footer: "Last Name",
-        accessor: "last_name",
-      },
-    ],
-  },
-  {
-    Header: "Info",
-    Footer: "Info",
-    accessor: [
-      {
-        Header: "Date of Birth",
-        Footer: "Date of Birth",
-        accessor: "date_of_birth",
-      },
-      {
-        Header: "Country",
-        Footer: "Country",
-        accessor: "country",
-      },
-      {
-        Header: "Phone",
-        Footer: "Phone",
-        accessor: "phone",
-      },
-    ],
-  },
-];
+// export const GROUPED_COLUMNS = [
+//   {
+//     Header: "Id",
+//     // Header 속성에는 테이블 헤더에 보여줄 텍스트를 명시
+//     Footer: "Id",
+//     accessor: "id",
+//     // accessor 속성에는 해당 열을 data 객체의 어느 속성을 읽어야하는지 명시
+//   },
+//   {
+//     Header: "Name",
+//     Footer: "Name",
+//     columns: [
+//       {
+//         Header: "First Name",
+//         Footer: "First Name",
+//         accessor: "first_name",
+//       },
+//       {
+//         Header: "Last Name",
+//         Footer: "Last Name",
+//         accessor: "last_name",
+//       },
+//     ],
+//   },
+//   {
+//     Header: "Info",
+//     Footer: "Info",
+//     accessor: [
+//       {
+//         Header: "Date of Birth",
+//         Footer: "Date of Birth",
+//         accessor: "date_of_birth",
+//       },
+//       {
+//         Header: "Country",
+//         Footer: "Country",
+//         accessor: "country",
+//       },
+//       {
+//         Header: "Phone",
+//         Footer: "Phone",
+//         accessor: "phone",
+//       },
+//     ],
+//   },
+// ];
